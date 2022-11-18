034837a17a539ba0e56e485f4d3186fbca92d520
RAJESHKUMARDSHADOW <rajeshkumarshadow@gmail.com>
2022-11-18 09:09:15 +0530

    next update

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -10,11 +10,20 @@ public class Git {
 		Git g = new Git();
 		
 		g.branch();
+		
+		//next update
+		g.nextbranch();
 	}
 	//worker
 	private void branch() {
 
 		System.out.println("Normal worker");
+	}
+	
+	//next update
+	private void nextbranch() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
