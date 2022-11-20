50879e1a1b421d0d5ff9d01b1d31f4b96dbea921
RAJESHKUMARDSHADOW <rajeshkumarshadow@gmail.com>
2022-11-20 13:36:07 +0530

    kumar code finished

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -2,10 +2,19 @@ package org.git;
 
 public class Git {
 
+	//master
 	public static void main(String[] args) {
-
+		//master
 		System.out.println("Hello World!");
 		
+		Git git1 = new Git();
+		git1.norandyCorporation();
+		
+	}
+	 //kumar code
+	private void norandyCorporation() {
+		System.out.println("wat you want is nothing");
+
 	}
 
 }
